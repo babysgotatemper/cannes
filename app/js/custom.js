6df144348ba9a6b390ffa6ea446dce06d1bdf2bc
RS <semak.roman@gmail.com>
2017-05-03 22:31:12 +0300

    main page + test

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,27 @@
-$(function() {
+$('._popup').hide();
+$('.main').show();
 
-	// Пользовательские функции
+$(function () {
+  //  popup
+  $('.popup-close').click(function () {
+    $(this).closest('._popup').fadeOut();
+  });
+
+  $('.test-start').click(function () {
+    $('.authorization').fadeIn();
+  });
+  //  auth
+  $('.social._auth').click(function () {
+    $(this).closest('._popup').fadeOut();
+    $('section').removeClass('active');
+    $('.test').addClass('active').find('.nomination').first().fadeIn();;
+  });
+  //test
+  $('.vote').click(function () {
+    $(this).closest('.nomination').hide().next('.nomination').fadeIn();
+    if ($(this).closest('.nomination').hasClass('auto')){
+      $('.test').fadeOut();
+    }
+  })
 
 });
