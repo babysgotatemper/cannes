197b1c36538dba2a23814baab7939de47a92f5be
RS <semak.roman@gmail.com>
2017-05-17 23:52:29 +0300

    php shanged

--- app/saveFB.php
+++ app/saveFB.php
@@ -1,7 +1,7 @@
 <?php
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata, true);
